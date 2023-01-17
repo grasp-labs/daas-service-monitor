277ccb755e2f512071b01f2880b3979c2d6c4703
Ubuntu <yuan@grasplabs.no>
2023-01-17 21:28:08 +0000

    format

--- backend/lambda_monitor/main.go
+++ backend/lambda_monitor/main.go
@@ -1,16 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"time"
-	"context"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
-    "github.com/aws/aws-sdk-go/service/lambda"
-    "github.com/aws/aws-sdk-go/service/cloudwatch"
+	"github.com/aws/aws-sdk-go/service/cloudwatch"
+	"github.com/aws/aws-sdk-go/service/lambda"
 
-	//"github.com/aws/aws-sdk-go-v2/service/lambda"
 	"github.com/aws/aws-lambda-go/events"
 	runtime "github.com/aws/aws-lambda-go/lambda"
 )
@@ -18,217 +17,189 @@ import (
 type Response events.APIGatewayProxyResponse
 
 type LambdaFunctionInfo struct {
-	Name string
-	InitMemory int64
-    Timeout int64
-	LastInvocationTime string
+	Name                   string
+	InitMemory             int64
+	Timeout                int64
+	LastInvocationTime     string
 	LastInvocationDuration float64
-    FunctionError bool
-    RunTimeoutError bool
+	FunctionError          bool
+	RunTimeoutError        bool
 }
 
 func GetFunctions() ([]string, error) {
 	sess, _ := session.NewSession(&aws.Config{
-        Region: aws.String("eu-north-1")},
-    );
+		Region: aws.String("eu-north-1")},
+	)
 
-    svc := lambda.New(sess)
+	svc := lambda.New(sess)
 
-	var funcs []string;
+	var funcs []string
 
-	var nextMarker *string;
-    for {
-        input := &lambda.ListFunctionsInput{
-            Marker: nextMarker,
-        }
-        resp, err := svc.ListFunctions(input)
-        if err != nil {
-            fmt.Println(err);
-            return funcs, err
-        }
+	var nextMarker *string
+	for {
+		input := &lambda.ListFunctionsInput{
+			Marker: nextMarker,
+		}
+		resp, err := svc.ListFunctions(input)
+		if err != nil {
+			fmt.Println(err)
+			return funcs, err
+		}
 
-        for _, function := range resp.Functions {
+		for _, function := range resp.Functions {
 			funcs = append(funcs, *(function.FunctionName))
-        }
+		}
 
-        if resp.NextMarker == nil {
-            break
-        }
+		if resp.NextMarker == nil {
+			break
+		}
 
-        nextMarker = resp.NextMarker;
-    }
+		nextMarker = resp.NextMarker
+	}
 
-	return funcs, nil;
+	return funcs, nil
 }
 
 func RetrieveLambdaFunctionInfo(funcName string) (*LambdaFunctionInfo, error) {
 
 	info := &LambdaFunctionInfo{
 		Name: funcName,
-	};
+	}
 
 	sess, _ := session.NewSession(&aws.Config{
-        Region: aws.String("eu-north-1")},
-    );
+		Region: aws.String("eu-north-1")},
+	)
 
 	// get lambda configuration info
 	lambdaClient := lambda.New(sess)
 
-    result, err := lambdaClient.GetFunction(&lambda.GetFunctionInput{
-        FunctionName: aws.String(funcName),
-    });
+	result, err := lambdaClient.GetFunction(&lambda.GetFunctionInput{
+		FunctionName: aws.String(funcName),
+	})
 
-    if err != nil {
-        fmt.Println("Failed to get function: ", err);
-        return nil, err;
-    };
+	if err != nil {
+		fmt.Println("Failed to get function: ", err)
+		return nil, err
+	}
 
-	info.InitMemory = *result.Configuration.MemorySize;
-    info.Timeout = *result.Configuration.Timeout;
+	info.InitMemory = *result.Configuration.MemorySize
+	info.Timeout = *result.Configuration.Timeout
 
-    cw := cloudwatch.New(sess)
+	cw := cloudwatch.New(sess)
 
 	startTime := time.Now().Add(-24 * time.Hour)
-    endTime := time.Now()
+	endTime := time.Now()
 
 	metricData, err := cw.GetMetricData(&cloudwatch.GetMetricDataInput{
-        MetricDataQueries: []*cloudwatch.MetricDataQuery{
-            {
-                Id: aws.String("m1"),
-                MetricStat: &cloudwatch.MetricStat{
-                    Metric: &cloudwatch.Metric{
-                        Namespace:  aws.String("AWS/Lambda"),
-                        MetricName: aws.String("Invocations"),
-                        Dimensions: []*cloudwatch.Dimension{
-                            {
-                                Name:  aws.String("FunctionName"),
-                                Value: aws.String(funcName),
-                            },
-                        },
-                    },
-                    Period: aws.Int64(60),
-                    Stat:   aws.String("Maximum"),
-                },
-                ReturnData: aws.Bool(true),
-            },
+		MetricDataQueries: []*cloudwatch.MetricDataQuery{
 			{
-                Id: aws.String("m2"),
-                MetricStat: &cloudwatch.MetricStat{
-                    Metric: &cloudwatch.Metric{
-                        Namespace:  aws.String("AWS/Lambda"),
-                        MetricName: aws.String("Errors"),
-                        Dimensions: []*cloudwatch.Dimension{
-                            {
-                                Name:  aws.String("FunctionName"),
-                                Value: aws.String(funcName),
-                            },
-                        },
-                    },
-                    Period: aws.Int64(60),
-                    Stat:   aws.String("Maximum"),
-                },
-                ReturnData: aws.Bool(true),
-            },
+				Id: aws.String("m1"),
+				MetricStat: &cloudwatch.MetricStat{
+					Metric: &cloudwatch.Metric{
+						Namespace:  aws.String("AWS/Lambda"),
+						MetricName: aws.String("Invocations"),
+						Dimensions: []*cloudwatch.Dimension{
+							{
+								Name:  aws.String("FunctionName"),
+								Value: aws.String(funcName),
+							},
+						},
+					},
+					Period: aws.Int64(60),
+					Stat:   aws.String("Maximum"),
+				},
+				ReturnData: aws.Bool(true),
+			},
 			{
-                Id: aws.String("m3"),
-                MetricStat: &cloudwatch.MetricStat{
-                    Metric: &cloudwatch.Metric{
-                        Namespace:  aws.String("AWS/Lambda"),
-                        MetricName: aws.String("Duration"),
-                        Dimensions: []*cloudwatch.Dimension{
-                            {
-                                Name:  aws.String("FunctionName"),
-                                Value: aws.String(funcName),
-                            },
-                        },
-                    },
-                    Period: aws.Int64(60),
-                    Stat:   aws.String("Maximum"),
-                },
-                ReturnData: aws.Bool(true),
-            },
-        },
-        StartTime: &startTime,
-        EndTime:   &endTime,
-    })
+				Id: aws.String("m2"),
+				MetricStat: &cloudwatch.MetricStat{
+					Metric: &cloudwatch.Metric{
+						Namespace:  aws.String("AWS/Lambda"),
+						MetricName: aws.String("Errors"),
+						Dimensions: []*cloudwatch.Dimension{
+							{
+								Name:  aws.String("FunctionName"),
+								Value: aws.String(funcName),
+							},
+						},
+					},
+					Period: aws.Int64(60),
+					Stat:   aws.String("Maximum"),
+				},
+				ReturnData: aws.Bool(true),
+			},
+			{
+				Id: aws.String("m3"),
+				MetricStat: &cloudwatch.MetricStat{
+					Metric: &cloudwatch.Metric{
+						Namespace:  aws.String("AWS/Lambda"),
+						MetricName: aws.String("Duration"),
+						Dimensions: []*cloudwatch.Dimension{
+							{
+								Name:  aws.String("FunctionName"),
+								Value: aws.String(funcName),
+							},
+						},
+					},
+					Period: aws.Int64(60),
+					Stat:   aws.String("Maximum"),
+				},
+				ReturnData: aws.Bool(true),
+			},
+		},
+		StartTime: &startTime,
+		EndTime:   &endTime,
+	})
 
-    if err != nil {
-        fmt.Println("Failed to get metric data", err)
-        return info, err;
-    }
+	if err != nil {
+		fmt.Println("Failed to get metric data", err)
+		return info, err
+	}
 
-    invocationTimestamps := metricData.MetricDataResults[0].Timestamps;
-    if len(invocationTimestamps) > 0 {
-        info.LastInvocationTime = invocationTimestamps[0].String();
-    }
+	invocationTimestamps := metricData.MetricDataResults[0].Timestamps
+	if len(invocationTimestamps) > 0 {
+		info.LastInvocationTime = invocationTimestamps[0].String()
+	}
 
-    durations := metricData.MetricDataResults[2].Values;
-    if len(durations) > 0 {
-        info.LastInvocationDuration = *(durations[0]) / 1000;
-        if (info.LastInvocationDuration > (float64)(info.Timeout + 1)) {
-            info.RunTimeoutError = true;
-        }
-    }
+	durations := metricData.MetricDataResults[2].Values
+	if len(durations) > 0 {
+		info.LastInvocationDuration = *(durations[0]) / 1000
+		if info.LastInvocationDuration > (float64)(info.Timeout+1) {
+			info.RunTimeoutError = true
+		}
+	}
 
-    errorTimestamps := metricData.MetricDataResults[1].Timestamps;
-    errorValues := metricData.MetricDataResults[1].Values;
-    if len(errorValues) > 0 && int(*errorValues[0]) > 0 {
-        lastError := errorTimestamps[0];
-        lastInvocation := metricData.MetricDataResults[0].Timestamps[0];
-        funcError := (*lastError == *lastInvocation) || lastError.After(*lastInvocation);
-        if funcError {
-            info.FunctionError = true;
-        }
-    }
+	errorTimestamps := metricData.MetricDataResults[1].Timestamps
+	errorValues := metricData.MetricDataResults[1].Values
+	if len(errorValues) > 0 && int(*errorValues[0]) > 0 {
+		lastError := errorTimestamps[0]
+		lastInvocation := metricData.MetricDataResults[0].Timestamps[0]
+		funcError := (*lastError == *lastInvocation) || lastError.After(*lastInvocation)
+		if funcError {
+			info.FunctionError = true
+		}
+	}
 
-    return info, nil;
+	return info, nil
 }
 
-
-
 func Handler(ctx context.Context) ([]*LambdaFunctionInfo, error) {
-	funcs, err := GetFunctions();
+	funcs, err := GetFunctions()
 	if err != nil {
-		fmt.Println(err);
+		fmt.Println(err)
 	}
 
-    var results []*LambdaFunctionInfo;
+	var results []*LambdaFunctionInfo
 
 	for _, func_name := range funcs {
-		info, err := RetrieveLambdaFunctionInfo(func_name);
+		info, err := RetrieveLambdaFunctionInfo(func_name)
 		if err != nil {
-			continue;
+			continue
 		}
-        results = append(results, info);
+		results = append(results, info)
 	}
 
-    return results, nil;
-
-    /*
-	body, err := json.Marshal(results);
-
-	if err != nil {
-		return Response{StatusCode: 404}, err
-	};
-    return body, nil
-    */
-    /*
-	json.HTMLEscape(&buf, body);
-
-    fmt.Println(buf);
-
-	resp := Response{
-		StatusCode:      200,
-		IsBase64Encoded: false,
-		Body:            buf.String(),
-		Headers: map[string]string{
-			"Content-Type":           "application/json",
-			"X-MyCompany-Func-Reply": "daas-service-monitor",
-		},
-	}
-
-	return resp, nil;
-    */
+	return results, nil
 }
 
 func main() {
