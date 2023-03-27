c5cd6496b5eba6318756a3cd922bc3d0a110aff3
mozhganpe66 <mozhgan.pe66@gmail.com>
2023-03-27 22:20:52 +0200

    add authentication feature
    add login logic
    add logout logic
    add routing
    add protected routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,64 +1,20 @@
-<<<<<<< Updated upstream
+
 import './App.css';
-
-import { useState, useEffect } from "react";
-
-import axios from "axios"
-
-import 'react-tabulator/lib/styles.css'; // required styles
-import 'react-tabulator/lib/css/tabulator.min.css'; // theme
-import { ReactTabulator } from 'react-tabulator';
+import Login from './components/Main/Login';
+import Logout from './components/Main/Logout';
+import Home from './components/Main/Home';
+import DashBoard from './components/Main/DashBoard';
+import ProtectedRoutes from './ProtectedRoutes';
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
 
 function App() {
 
-  const [rows, setRows] = useState([]);
 
-  var config = {
-    method: 'get',
-    headers: { 
-      'x-api-key': process.env.REACT_APP_API_KEY, 
-      'Accept': 'application/json',
-    }
-  };
 
-  useEffect( () => {
-    const fetchMetrics = async () => {
-      const rootUrl = process.env.NODE_ENV === "production" ? "https://ollq6b7h96.execute-api.eu-north-1.amazonaws.com" : ""
-      await axios.get(`${rootUrl}/dev/monitor/lambda/`, config)
-      .then(response => {
-        setRows(response.data.body);
-    })
-      .catch(err => console.error);
-    };
-    fetchMetrics();
-  }, []);
-
-  const columns = [
-    { title: "Name", field: "Name", width: 150 },
-    { title: "InitMemory (G)", field: "InitMemory", width: 150},
-    { title: "Timeout (s)", field: "Timeout", width: 150},
-    { title: "LastInvocationTime", field: "LastInvocationTime", width: 150},
-    { title: "Duration (s)", field: "LastInvocationDuration", width: 150},
-    { title: "Success", field: "Success", hozAlign: "center", formatter: "tickCross", width: 150},
-    { title: "Error", field: "Error", hozAlign: "center"},
-  ]
 
   return (
-    <div className="App">
-      <h1>DAAS Service Monitor</h1>
-      <ReactTabulator columns={columns} data={rows}/>
-=======
-import {React} from 'react'
-import Home from  './components/Main/Home'
-import DashBoard from './components/Main/DashBoard'
-import Login from './components/Main/Login'
-import { BrowserRouter , Routes, Route, Link} from 'react-router-dom'
-import ProtectedRoutes from './ProtectedRoutes'
-import Logout from './components/Main/Logout'
-
-function App() {
-return (
-   <div>
+  <div>
+    
     <BrowserRouter>
     <header className="p-3 text-bg-dark">
     <div className="container">
@@ -74,7 +30,7 @@ return (
         </div>
         
       </div>
->>>>>>> Stashed changes
+
     </div>
   </header>
 
