22b382465b472d2a061c66e16b89442083ae6342
mozhganpe66 <mozhgan.pe66@gmail.com>
2023-05-01 21:33:09 +0200

    updating jest configuration file

--- jest.config.js
+++
@@ -1,3 +0,0 @@
-export const roots = ["<rootDir>/src"];
-export const testMatch = ["**/__tests__/**/*.+(js|jsx|ts|tsx)", "**/?(*.)+(spec|test).+(js|jsx|ts|tsx)"];
-export const moduleFileExtensions = ["js", "jsx", "ts", "tsx", "json", "node"];
---
+++ jest.config.jsx
@@ -0,0 +1,13 @@
+export const testEnvironment = 'node';
+export const testMatch = ['**/*.test.jsx'];
+export const moduleFileExtensions = ['jsx'];
+export const coverageReporters = ['text-summary', 'html'];
+export const collectCoverageFrom = ['**/*.jsx'];
+export const coverageThreshold = {
+    global: {
+        statements: 80,
+        branches: 80,
+        functions: 80,
+        lines: 80
+    }
+};
