0628bd7a0a8186af73dd510595d8f0dee60f55b3
mozhganpe66 <mozhgan.pe66@gmail.com>
2023-05-01 21:14:48 +0200

    Create jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    roots: ["<rootDir>/src"],
+    testMatch: ["**/__tests__/**/*.+(js|jsx|ts|tsx)", "**/?(*.)+(spec|test).+(js|jsx|ts|tsx)"],
+    moduleFileExtensions: ["js", "jsx", "ts", "tsx", "json", "node"],
+  }
